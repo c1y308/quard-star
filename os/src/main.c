c99230aa2d88abe3ed380ae5938950f8ecd10d5b
c1y308 <755381914@qq.com>
2025-09-22 22:23:06 +0800

    完成内核恒等映射，开启调试功能

--- os/src/main.c
+++ os/src/main.c
@@ -1,19 +1,30 @@
 #include <timeros/os.h>
 #include <timeros/assert.h>
-extern void frame_allocator_test();
+extern void frame_alloctor_init();
+extern void kvminit();
+extern void kvminithart();
 void os_main()
 {
    printk("hello timer os!\n");
-   // frame_allocator_test();
-   // while (1)
-   // {
-   //    /* code */
-   // }
+
+   // 内存分配器初始化
+   frame_alloctor_init();
+
+   kvminit();
+
+   
+   kvminithart();
+
    trap_init();
+
+
+   
 
    task_init();
 
    timer_init();
+
    
-   run_first_task();
+   // run_first_task();
+
 }
